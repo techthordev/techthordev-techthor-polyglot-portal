7fbcf92b39b08c1aa29888461cc875a404285351
tfeydev <tfeydeveloper@gmail.com>
2025-12-04 15:09:27 -0300

    fixing navigation and refresh

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideClientHydration } from '@angular/platform-browser';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(
-      routes 
-    ),
-    provideClientHydration()
+    provideRouter(routes),                              // Provide the router with your routes
+    { provide: LocationStrategy, useClass: HashLocationStrategy }, // Use hash-based routing
+    provideClientHydration()                            // Client hydration if you need it
   ]
 };
