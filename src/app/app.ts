2f7666fece14fc1b7705e58892754c2ed250311f
tfeydev <tfeydeveloper@gmail.com>
2025-12-04 08:08:56 -0300

    improvement, refactoring

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { Banner } from './banner/banner';
+import { Banner } from './layout/banner/banner';
+import { Footer } from './layout/footer/footer';
+import { Content } from './layout/content/content';
 
 @Component({
   selector: 'app-root',
-  imports: [Banner],
+  imports: [Banner, Footer, Content],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
