6553b17d957b17df2f5c1144c632931693b4a98b
tfeydev <tfeydeveloper@gmail.com>
2025-12-07 23:14:36 -0300

    add /projects

---
+++ src/app/data/projects.data.ts
@@ -0,0 +1,30 @@
+import { ProjectItem } from '../features/projects/projects';
+
+export const PROJECT_DATA: ProjectItem[] = [
+  {
+    name: 'Polyglot Portal',
+    category: 'Portal Showcase',
+    description:
+      'High‑gloss reference site built with Angular 21+ and TailwindCSS v4+. It presents stacks, tracks, and ideas in a professional, unified way. Still evolving, but already a showcase of modern web design and implementation.',
+    iconPath: 'assets/icons/angular-logo.png',
+    repoUrl: 'https://github.com/techthordev/techthor-polyglot-portal',
+  },
+  {
+    name: 'LeetCode Database Extension',
+    category: 'Proof of Work',
+    description:
+      'Dedicated GitHub repository and GitHub Page (techthordev.github.io/leetcode). Spring Boot backend (Docker/Podman on OCI VM) reads SQL solution files from GitHub, generates API endpoints, executes queries against PostgreSQL, and returns results as JSON. Frontend built with Angular 21+ and TailwindCSS v4+.',
+    iconPath: 'assets/icons/postgresql-logo.png',
+    repoUrl: 'https://techthordev.github.io/leetcode',
+  },
+];
+
+export const getGroupedProjects = (data: ProjectItem[]) => {
+  return data.reduce(
+    (acc, item) => {
+      (acc[item.category] = acc[item.category] || []).push(item);
+      return acc;
+    },
+    {} as Record<ProjectItem['category'], ProjectItem[]>,
+  );
+};
--- src/app/features/projects/projects.ts
+++ src/app/features/projects/projects.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PROJECT_DATA, getGroupedProjects } from '../../data/projects.data';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
 export class Projects {
+  projects = PROJECT_DATA;
+  groupedProjects = getGroupedProjects(this.projects);
+}
 
+export interface ProjectItem {
+  name: string;
+  description: string;
+  iconPath: string;
+  repoUrl?: string;
+  category: 'Portal Showcase' | 'Proof of Work';
 }
